4824ce1418cef0feda86b988277d1caa4f4eac08
Brinta3 <brinta728@gmail.com>
2025-07-10 20:27:42 +0600

    Create Celsius _to_Fahrenheit.cpp

---
+++ Celsius _to_Fahrenheit.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+double Fer, Cel =25.8;
+Fer=(9/5 * Cel)+32;
+cout<<"The Temperature of 25.8 degree Celsius is :"<<endl;
+cout<<Fer<<" degree Fahrenheit";
+return 0;
+}
