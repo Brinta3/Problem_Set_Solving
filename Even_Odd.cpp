0ba4b7f4320674ad4bd9d3893f56a4346b473af7
Brinta3 <brinta728@gmail.com>
2025-07-10 21:03:49 +0600

    Create Even_Odd.cpp

---
+++ Even_Odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"Enter an integer:";
+cin>>a;
+if(a%2==0){
+    cout<<"Number is Even";
+}
+else{
+    cout<<"number is Odd";
+}
+return 0;
+}
