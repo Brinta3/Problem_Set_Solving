026adcb4c825061268bd69c8d9cb8b93d5392846
Brinta3 <brinta728@gmail.com>
2025-07-12 22:23:17 +0600

    Create Triangle_Area.cpp

---
+++ Triangle_Area.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+double height, base, area;
+cout<<"Enter height of triangle:";
+cin>>height;
+cout<<"Enter base of triangle:";
+cin>>base;
+area=0.5*height*base;
+cout<<"The area of triangle is:"<<area;
+return 0;
+}
